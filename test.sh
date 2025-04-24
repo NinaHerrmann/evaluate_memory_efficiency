f3a8a20b5c417133b52554933ca78de1252a5a3f
Nina Herrmann <ninaherrmann@D-3170L10.WIWI.UNI-MUENSTER.DE>
2025-04-24 19:45:01 +0200

    increased number of test

--- test.sh
+++ test.sh
@@ -5,4 +5,4 @@
 #types=("binary" "binary" "multiclass" "regression" "regression" "regression" ) #"multiclass"
 
 # Loop through each dataset and its corresponding type
-python3 Experiments.py --iterations 5 --nrs 5  --train > data/$dataset/$dataset.out
+python3 Experiments.py --iterations 1000 --nrs 10  --train > data/$dataset/$dataset.out
