35369d90ae2eece28a36d3a0067890597d414c9c
n_herr03 <nina.herrmann@uni-muenster.de>
2025-04-28 09:31:24 +0200

    palma changes

--- test.sh
+++ test.sh
@@ -5,4 +5,4 @@
 #types=("binary" "binary" "multiclass" "regression" "regression" "regression" ) #"multiclass"
 
 # Loop through each dataset and its corresponding type
-python3 Experiments.py --iterations 1000 --nrs 10  --train > data/$dataset/$dataset.out
+python3 Experiments.py --iterations 100 --nrs 10  --train > /scratch/tmp/n_herr03/ecai/outdata/$dataset.out
